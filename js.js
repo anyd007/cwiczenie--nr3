cbf6ba63baf23bbbf05d90269a5c11cee431ee4e
Andrzej Gebala <andrzej.gebala35@gmail.com>
2021-10-05 20:52:23 +0200

    dodanie funkcji na imputa, dodanie przycisku zapis

---
+++ js.js
@@ -0,0 +1,42 @@
+const pass = document.querySelector('#password2');
+const p = document.querySelector('.passinfo');
+const copyBtn = document.querySelector('#showPass');
+const letters = /[a-z]/i;
+const numbers = /[0-9]/;
+const special = /[!@#$%^&*()]/;
+const minValue = 10;
+
+const newPass = () =>{
+    if(pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers) && pass.value.match(special)){
+        p.textContent = 'twoje hasło bije inne hałsa na głowę....i nie jest to 1234'
+    } else if(pass.value.length > minValue && pass.value.match(letters) && pass.value.match(numbers)){
+        p.textContent =  'jesteś blsko idealnego hasła....dodaj znak specjalny'
+    } else {
+        p.textContent = 'dasz radę...stać cię na lepsze hasło'
+    }
+}
+
+const checkPass = () =>{
+    if(pass.value === ''){
+        p.textContent = 'pole wyżej nie może być puste'
+    }else{
+        newPass();
+    }
+    p.style.textTransform = 'uppercase';
+}
+
+const copy = () =>{
+    
+    if(pass.value !== ''){
+        pass.select();
+    document.execCommand('copy');
+    copyBtn.innerHTML = 'skopiowane'
+} else{
+    copyBtn.innerHTML = 'napisz coś'
+        
+    }
+}
+
+
+pass.addEventListener('keyup', checkPass);
+copyBtn.addEventListener('click', copy);
